c6236bd9483c8d3ef71d6275f17e7246b5cc377e
marmooo <marmooo@users.noreply.github.com>
2022-05-02 00:17:12 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = '2022-04-19 00:10';
+var CACHE_NAME = "2022-05-02 00:15";
 var urlsToCache = [
   "/kanji-siritori/",
   "/kanji-siritori/2/",
   "/kanji-siritori/index.js",
   "/kanji-siritori/2.json",
   "/kanji-siritori/3.json",
-  "/kanji-siritori/favicon/original.svg",
+  "/kanji-siritori/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
 ];
 
